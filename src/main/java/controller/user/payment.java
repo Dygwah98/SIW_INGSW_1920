36c3ea3fc57d3def3687a24a85a04bfa076558cd
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2020-02-02 17:16:42 +0100

    Update payment.java

--- src/main/java/controller/user/payment.java
+++ src/main/java/controller/user/payment.java
@@ -26,39 +26,6 @@ public class payment extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 4725397173656796295L;
-
-	private Integer processProducts(Integer id_ordine) {
-		
-		try {
-			Integer s = 0;
-			ProdottoDao f = DBManager.getInstance().getDAOFactory().getProdottoDao();
-			List<ProdottoAggregato> prodc = f.showProductsForCart(id_ordine);
-			for(ProdottoAggregato p : prodc)
-				s += p.getPrezzo();
-			
-			return s;
-		
-		} catch(NullPointerException e) {
-			return 0;
-		}
-	}
-	
-	private Integer processRooms(Integer id_user) {
-		
-		try {
-			Integer s = 0;
-			OrdineDao d = DBManager.getInstance().getDAOFactory().getOrdineDao();
-			List<Integer> prezzi = d.retrievePrezzoCamere(id_user);
-			
-			for(Integer i : prezzi)
-				s += i;
-			
-			return s;
-			
-		} catch(NullPointerException e) {
-			return 0;
-		}
-	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -66,26 +33,18 @@ public class payment extends HttpServlet {
 
 		DAOFactory factory = DBManager.getInstance().getDAOFactory();
 
-		Integer total = 0;
-		total += processProducts((Integer) request.getSession().getAttribute("idordine"));
-		total += processRooms((Integer) request.getSession().getAttribute("userId"));
-
-		if (total <= 0) {
-			response.setStatus(412);
-		} else {
-			Date d = new Date(new java.util.Date().getTime());
+		Date d = new Date(new java.util.Date().getTime());
 			
-			Pagamento p = new Pagamento();
-			p.setIdOrdine((Integer) request.getSession().getAttribute("idordine"));
-			p.setDataPagamento(d);
-			p.setImporto(total);
+		Pagamento p = new Pagamento();
+		p.setIdOrdine((Integer) request.getSession().getAttribute("idordine"));
+		p.setDataPagamento(d);
+		p.setImporto((Integer) request.getSession().getAttribute("totp"));
 		
-			factory.getPagamentoDao().save(p);
+		factory.getPagamentoDao().save(p);
+
+		factory.getOrdineDao().pay((Integer) request.getSession().getAttribute("userId"));
 			
-			factory.getOrdineDao().pay((Integer) request.getSession().getAttribute("userId"));
-			
-			response.setStatus(201);
-		}
+		response.setStatus(201);
 	}
 
 	@Override
