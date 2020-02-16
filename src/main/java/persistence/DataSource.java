0dc1352f020133d972cb216152e1f603fab60ef3
misterx98 <48914047+misterx98@users.noreply.github.com>
2020-02-16 15:39:48 +0100

    Update DataSource.java

--- src/main/java/persistence/DataSource.java
+++ src/main/java/persistence/DataSource.java
@@ -6,15 +6,20 @@ public class DataSource {
 	private String dbURL; // = "jdbc:postgresql://sarella.cqenbowd50kg.eu-central-1.rds.amazonaws.com";
 	private String userName; // = "riuzaki9797";
 	private String password; // = "*Francesco1.,";
+	private Connection connection;
 
 	public DataSource(String dbURI, String userName, String password) {
 		this.dbURL = dbURI;
 		this.userName = userName;
 		this.password = password;
+		this.connection = null;
 	}
 
 	public Connection getConnection() throws SQLException {
-		Connection connection = DriverManager.getConnection(dbURL, userName, password);
+		if (connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(dbURL, userName, password);
+			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+		}
 		return connection;
 	}
 }
